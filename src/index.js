a07c0ead8e8c9a2e8dd37382776414a2daeb0b93
Jarred 'Beatnik' Hauschild <contact@beatnik.id.au>
2017-08-15 17:20:34 +0100

    Implemented hourly removal of automated mutes, implemented one hour new member role assignment and link posting restrictions for new members

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const CONFIG = require(`../config.json`);
+const CONFIG = require(`../configTest2.json`);
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+var timerRun = false;
+
 client.on(`message`, message => {
 
+  //Showcase channel functions
   if (message.channel.type === `text` && message.guild.id === CONFIG.guild && message.channel.id === CONFIG.reactionChannel) {
     var content = message.content;
     if (/http:/i.test(content) || /https:/i.test(content)) {
@@ -15,15 +18,63 @@ client.on(`message`, message => {
       message.react("😍");
     }
 
-    //if content.search(/<:ResidentSleeper:252374036976369664>/i) {
     if (/ResidentSleeper/.test(content) || /👎/.test(content) || /FailFish/.test(content)) {
       message.delete()
-        .then(msg => console.log(`Deleted message from ${msg.author}`))
+        .then(msg => console.log(`Deleted message from ${message.author} - ${message.author.username}`))
         .catch(console.error);
+    }
+  }
+
+  //General channel functions
+  if (message.channel.type === `text` && message.guild.id === CONFIG.guild && message.channel.id === CONFIG.generalChannel) {
+    var content = message.content;
+    var author = message.author;
+    var newMember = client.guilds.get(CONFIG.guild).members.get(author.id).roles.find('name', 'New Member');
+
+      //New Member restrictions
+      if (newMember != null && /http/i.test(content)) {
+        message.delete()
+          .then(msg => console.log(`Deleted message from New Member ${author} - ${author.username}`))
+          .then(message.reply(`sorry. New members cannot post links in #general for the first hour after joining.`))
+          .catch(console.error);
+      }
+  }
+
+  //Announcement scheduling functions
+/*  if (message.channel.type === `text` && message.guild.id === CONFIG.guild && message.channel.id === CONFIG.announceChannel) {
+    var content = message.content;
+
+  }
+*/
+  //Welcome channel (new member) functions
+  if (message.channel.type === `text` && message.guild.id === CONFIG.guild && message.channel.id === CONFIG.welcomeChannel) {
+    var content = message.content;
+    if (/\.iam member/i.test(content)) {
+      var member = message.author.id;
+      var server = client.guilds.get(CONFIG.guild);
+      var role = server.roles.find('name', 'New Member').id;
+      if (role != null) {
+        server.members.get(member).addRole(role);
+        setTimeout(function() {
+          server.members.get(member).removeRole(role);
+        }, 3600000);
+      }
     }
   }
 
 });
 
-client.on(`ready`, () => console.log(`${client.user.username}: I'm ready!`));
+//Remove BotMuted role hourly
+setInterval( function() {
+  var d = new Date();
+  var server = client.guilds.get(CONFIG.guild);
+  if (d.getMinutes() === 5 && server.roles.find('name', 'BotMuted') != null) {
+    var role = server.roles.find('name', 'BotMuted').delete();
+  }
+}, 60000);
+
+client.on(`ready`, () => {
+  console.log(`${client.user.username}: I'm ready!`);
+});
+
 client.login(CONFIG.token);
